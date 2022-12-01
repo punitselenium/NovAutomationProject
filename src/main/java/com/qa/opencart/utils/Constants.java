a164df3a798977cd809c1ec50249a508de371a59
punitselenium <46515623+punitselenium@users.noreply.github.com>
2022-12-01 15:31:39 +0530

    Rdp new changes (#2)
    * Added new test case on RDP
    
    * Total Restaurant Image Link Change

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -7,8 +7,6 @@ public class Constants {
 	public static final String LOGIN_PAGE_URL_VALUE = "dineout.co.in";
 	public static final String RESTAURANT_DETAIL_PAGE_TITLE = "Tamasha, Connaught Place, Central Delhi, Delhi | Dineout";
 	public static final String RESTAURANT_NAME_IS = "Tamasha";
-	public static final String RDP_LINK_VALUE = "href";
-	public static final String RDP_SRC_VALUE = "src";
-
-
+	public static final String RDP_LINK_VALUE = "title";
+	public static final String RDP_SRC_VALUE = "data-src";
 }
